d1c18413baec0a50c1afc2bdee722e2d5ad61bb5
gggokopannn <120161554+gggokopannn@users.noreply.github.com>
2025-08-28 10:26:49 +0700

    add articles list

---
+++ app/articles/page.tsx
@@ -0,0 +1,16 @@
+import * as types from "@/models/article";
+interface ArticlesPageProps {
+  searchParams: Promise<{
+    page?: number;
+  }>;
+}
+
+const ArticlesPage = async (params: ArticlesPageProps) => {
+  const searchParams = await params.searchParams;
+  const page = searchParams.page || 1;
+  const url = `${process.env.NEXT_PUBLIC_API_URL}/products?page=${page}`;
+
+  const res = await fetch(url);
+  const data = await (res.json() as Promise<types.Article>);
+};
+export default ArticlesPage;
---
+++ components/articles-list/articles-list.tsx
@@ -0,0 +1,13 @@
+import * as types from "@/models/article";
+
+const ArticleList = ({ items: article, paging }: types.ArticleList) => {
+  return (
+    <>
+        {article.map((article) => (
+  
+        ))}
+    </>
+  );
+};
+
+export default ArticleList;
